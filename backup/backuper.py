84e902c586d204f1007db19d4e9e6ac9d581d577
akindyakov <akindyakov@gmail.com>
2015-09-27 18:38:37 +0300

    add rsync'er to remote host

--- backup/backuper.py
+++ backup/backuper.py
@@ -1,41 +1,55 @@
 #!/usr/bin/env python3
 
 import argparse
-import logging
-import sys
-import hashlib
 import datetime
+import logging
+import os
 import subprocess
+import sys
+import tempfile
 
 log = logging.getLogger(__name__)
+
+storage = {
+    "index": ["phobos", "deimos"],
+}
 
 cfg = {
     "akindyakov_source": {
         "local": True,
-        "path": "/home/akindyakov/bare.repo",
-        "tail_size": 14,
+        "filename": "akindyakov_source.%Y-%m-%d",
+        "path": "/home/akindyakov/bare.repo", "tail_size": 14,
         "time_interval": datetime.timedelta(days=1),
-        "storage": {
-            "index": ["phobos", "deimos"],
-        },
     },
     "akindyakov_photo": {
-        "inplace": True,
+        "tail_size": 0,  # inplace
         "local": False,
+        "path": "/home/akindyakov/data/photo",
+        "remote": {
+            "host": "192.168.1.215",
+            "port": 6013,
+        },
     },
     "akindyakov_doc": {
-        "inplace": True,
+        "tail_size": 0,  # inplace
         "local": False,
+        "path": "/home/akindyakov/data/documents",
+        "remote": {
+            "host": "192.168.1.215",
+            "port": 6013,
+        },
     },
     "akindyakov_video": {
-        "inplace": True,
+        "tail_size": 0,  # inplace
         "local": False,
-    },
-    "akindyakov_video": {
-        "inplace": True,
-        "local": False,
+        "path": "/home/akindyakov/data/video",
+        "remote": {
+            "host": "192.168.1.215",
+            "port": 6013,
+        },
     },
 }
+
 
 class TRsyncError(Exception):
     def __init__(self, returncode):
@@ -44,18 +58,24 @@ class TRsyncError(Exception):
     def __repr__(self):
         return "TRsyncError(returncode={})".format(self.returncode)
 
-def rsync(from_path, to_path, opts):
-    cmd = ["rsync"]
-    cmd += opts
-    cmd.append(from_path)
-    cmd = " ".join(cmd)
-    log.info("Popen: [%s]", cmd)
-    with subprocess.Popen(
-            cmd,
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        ) as proc:
+class TTarError(Exception):
+    def __init__(self, returncode):
+        self.returncode = returncode
+
+    def __repr__(self):
+        return "TTarError(returncode={})".format(self.returncode)
+
+
+def irsync(from_path, to_path, opts):
+    log.error("rsync: [%s]", from_path)
+    cmd = " ".join([
+        "rsync",
+        opts,
+        from_path,
+        to_path,
+    ])
+    log.debug("Popen: [%s]", cmd)
+    with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
         for line in proc.stdout:
             yield line
         for line in proc.stderr:
@@ -64,45 +84,111 @@ def rsync(from_path, to_path, opts):
         if proc.returncode != 0:
             raise TRsyncError(proc.returncode)
 
-def check_time():
+
+def pack(path, tfile):
+    """
+        tar --create path --gzip --file tfile
+    """
+    cmd = [
+        "tar",
+        "--create", path,
+        "--gzip",
+        "--absolute-names",
+        "--file", tfile,
+    ]
+    log.debug(" ".join(cmd))
+    with subprocess.Popen(" ".join(cmd), shell=True, stderr=subprocess.PIPE) as proc:
+        for line in proc.stderr:
+            log.debug("Popen stderr: %s", line)
+        proc.wait()
+        if proc.returncode != 0:
+            raise TTarError(proc.returncode)
+
+
+def implace_backup(args):
     pass
 
-def check():
-    pass
+
+def tail_backup(args):
+    with tempfile.NamedTemporaryFile(mode='w+b', prefix="tmp.", suffix='.tar.gz') as tmp:
+        pack(args.what, tmp.name)
+        fname = datetime.datetime.now().strftime(args.backup_file_fmt) + ".tar.gz"
+        dst = "{}:{}".format(
+            args.backup_host,
+            os.path.join(args.backup_path, fname),
+        )
+        log.debug("%s", dst)
+        log.debug("%s", tmp.name)
+        log.debug("%s", """--rsh='ssh -p {}'""".format(args.backup_port))
+        for _ in irsync(tmp.name, dst, """--rsh='ssh -p {}'""".format(args.backup_port)): pass
+
 
 def main():
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument(
-        '--backup-server-host',
-        type=str,
-        help=''
-    )
-    parser.add_argument(
-        '--backup-server-port',
-        type=str,
-        help=''
-    )
-    parser.add_argument(
-        '--backup-server-path',
-        type=str,
-        help=''
-    )
-    parser.add_argument(
-        '-l', '--log',
-        type=str,
-        metavar="FILE",
-        help='Custom log file name',
-    )
-    args = parser.parse_args()
-
+    args = arguments()
     if args.log is not None:
         log.debug("add custom log file")
-        fh = logging.FileHandler(args.log)
-        fh.setLevel(logging.DEBUG)
-        fh.setFormatter(
-            logging.Formatter('%(asctime)s %(name)s [%(process)d] %(levelname)s %(message)s')
+        filehandler = logging.FileHandler(args.log)
+        filehandler.setLevel(logging.DEBUG)
+        filehandler.setFormatter(
+            logging.Formatter("%(asctime)s %(name)s [%(process)d] %(levelname)s %(message)s")
         )
-        log.addHandler(fh)
+        log.addHandler(filehandler)
+
+    if args.tail_size == 0:
+        implace_backup(args)
+    else:
+        tail_backup(args)
+
+
+def arguments():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "--what",
+        required=True,
+        metavar="PATH",
+        type=str,
+        help="Backuping directory",
+    )
+    parser.add_argument(
+        "--backup-host",
+        type=str,
+        help=""
+    )
+    parser.add_argument(
+        "--backup-port",
+        type=str,
+        help=""
+    )
+    parser.add_argument(
+        "--backup-path",
+        required=True,
+        type=str,
+        help=""
+    )
+    parser.add_argument(
+        "--local",
+        action="store_true",
+        help="Local mode",
+    )
+    parser.add_argument(
+        "--backup-file-fmt",
+        type=str,
+        required=True,
+        help=".tar.gz file fmt",
+    )
+    parser.add_argument(
+        "--tail-size",
+        type=int,
+        default=0,
+        help="Tail size for filestorage. Default: %(default)s",
+    )
+    parser.add_argument(
+        "--log",
+        type=str,
+        metavar="FILE",
+        help="Custom log file name",
+    )
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
@@ -113,14 +199,11 @@ if __name__ == "__main__":
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
         ch.setFormatter(
-            logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+            logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s")
         )
         log.addHandler(ch)
 
         main()
-        for l in rsync("192.168.1.215:rtorrent", "", ["""--rsh='ssh -p 6013'"""]):
-            log.debug(l)
-        #rsync("/home/", "", [])
 
     except SystemExit:
         raise
