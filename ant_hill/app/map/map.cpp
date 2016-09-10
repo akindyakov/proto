391a7ca8fbbeccc7e27a8f3675bb546f7c7bbb81
akindyakov <akindyakov@gmail.com>
2016-09-10 22:48:54 +0300

    Connect lib/map/libmap.a to app/map/map

--- ant_hill/app/map/map.cpp
+++ ant_hill/app/map/map.cpp
@@ -1,6 +1,36 @@
+#include <lib/map/2d_field.h>
+
+#include <tools/tests/ut.h>
+
 #include <iostream>
 
+void App() {
+    std::string text = R"FieldMap(10
+10
+iiiiiiiiii
+is......wi
+i.s....w.i
+i..s..w..i
+i...sw...i
+i...ws...i
+i..w.ms..i
+i.w.m..s.i
+iw...m..si
+iiiiiiiiii
+)FieldMap";
+    auto in = std::istringstream(text);
+    auto field = NField::ScanFromText(in);
+    auto out = std::ostringstream();
+    NField::PrintToText(out, field);
+    auto outText = out.str();
+    if (text != outText) {
+        throw NAntHill::TException()
+            << "Expected: " << text
+            << "\nGot: " << outText;
+    }
+}
+
 int main() {
-    std::cout << "Hello Alex!" << std::endl;
+    App();
     return 0;
 }
