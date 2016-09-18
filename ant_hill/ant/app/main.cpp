42e0b6fd47074968b3ddd79e959d24b0e37750a0
akindyakov <akindyakov@gmail.com>
2016-09-18 21:04:45 +0300

    Ping-pong boilerplate for (ant --> map) level

---
+++ ant_hill/ant/app/main.cpp
@@ -0,0 +1,33 @@
+#include <map/rpc/client.h>
+
+#include <jsonrpccpp/client/connectors/httpclient.h>
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+int main()
+{
+    // TODO: Put arguments parsing here
+    using namespace std::chrono_literals;
+    auto httpclient = jsonrpc::HttpClient("http://localhost:8383");
+    auto client = NMap::NJsonRPC::TClient{httpclient};
+    try {
+        std::cout << "GetGrain(56, 34): " << client.GetGrain(56, 34) << std::endl;
+        while (true) {
+            std::this_thread::sleep_for(1s);
+
+            auto start = std::chrono::high_resolution_clock::now();
+            if (client.Ping() != 0) {
+                throw std::exception();
+            }
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::milli> elapsed = end - start;
+            std::cout << "Ping: " << elapsed.count() << "ms" << std::endl;
+        }
+    }
+    catch (const jsonrpc::JsonRpcException& err)
+    {
+        std::cerr << err.what() << std::endl;
+    }
+}
---
+++ ant_hill/map/app/main.cpp
@@ -0,0 +1,44 @@
+#include <map/rpc/server.h>
+
+#include <jsonrpccpp/server/connectors/httpserver.h>
+
+#include <iostream>
+
+class TMapServer
+    : public NMap::NJsonRPC::TServer
+{
+    public:
+        TMapServer(jsonrpc::AbstractServerConnector &conn)
+            : NMap::NJsonRPC::TServer(conn)
+        {
+        }
+
+        int GetGrain(int x, int y) override {
+            std::cerr << "GetGrain(" << x << ", " << y << ")" << std::endl;
+            // TODO:
+            return x + y;
+        }
+        int MoveGroup(const Json::Value& grains) override {
+            std::cerr << "MoveGroup(...)" << std::endl;
+            // TODO:
+            // TODO:
+            return 0;
+        }
+        int Ping() override {
+            std::cerr << "Ping()" << std::endl;
+            // TODO:
+            return 0;
+        }
+};
+
+
+int main()
+{
+    // TODO: Put argument parsing here
+    jsonrpc::HttpServer httpserver(8383);
+    TMapServer s(httpserver);
+    s.StartListening();
+    std::cin.get();
+    s.StopListening();
+    return 0;
+}
