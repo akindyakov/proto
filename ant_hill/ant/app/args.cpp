6aea550196560ee4d33d7c3940320a306bd73997
akindyakov <akindyakov@gmail.com>
2016-11-13 00:43:30 +0300

    add [name] cmd option for ant app

--- ant_hill/ant/app/args.cpp
+++ ant_hill/ant/app/args.cpp
@@ -33,13 +33,22 @@ std::tuple<NArgs::variables_map, bool> Argparse(int argn, char** argv) {
         )
     ;
 
+    auto unit = NArgs::options_description{"Ant unit configuration"};
+    unit.add_options()
+        (
+            "name",
+            NArgs::value<std::string>(),
+            "Unit unique name"
+        )
+    ;
+
     auto hidden = NArgs::options_description{"Hidden options"};
     hidden.add_options()
         ("input-file", NArgs::value<std::vector<std::string>>(), "input file")
     ;
 
     auto cmdline_options = NArgs::options_description{};
-    cmdline_options.add(generic).add(config).add(hidden);
+    cmdline_options.add(generic).add(config).add(unit).add(hidden);
 
     auto args = NArgs::variables_map{};
     NArgs::store(NArgs::parse_command_line(argn, argv, cmdline_options), args);
