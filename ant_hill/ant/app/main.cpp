8c819faf9466ae30df31bae5484a44c4d8436f6a
akindyakov <akindyakov@gmail.com>
2017-05-07 22:19:35 +0300

    appear and step for ant lyfe cycle

--- ant_hill/ant/app/main.cpp
+++ ant_hill/ant/app/main.cpp
@@ -1,5 +1,7 @@
 #include "args.h"
 
+#include <ant/lib/ant.h>
+
 #include <map/rpc/client.h>
 
 #include <jsonrpccpp/client/connectors/httpclient.h>
@@ -19,14 +21,14 @@ int main(int argn, char** argv) {
     using namespace std::chrono_literals;
     auto httpclient = jsonrpc::HttpClient("http://localhost:8383");
     auto client = Map::JsonRPC::Client{httpclient};
+    auto ant = Scout(client);
     try {
+        ant.appear();
         while (true) {
             std::this_thread::sleep_for(1s);
 
             auto start = std::chrono::high_resolution_clock::now();
-            if (client.Ping() != 0) {
-                throw std::exception();
-            }
+            ant.step();
             auto end = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double, std::milli> elapsed = end - start;
             std::cout << "Ping: " << elapsed.count() << "ms" << std::endl;
