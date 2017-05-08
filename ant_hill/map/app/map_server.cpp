ab508353ca4120f8dfb6a911ac0efbd41be81605
akindyakov <akindyakov@gmail.com>
2017-05-08 17:12:59 +0300

    Exeption reporting

--- ant_hill/map/app/map_server.cpp
+++ ant_hill/map/app/map_server.cpp
@@ -5,7 +5,7 @@
 
 #include <map/rpc/server.h>
 
-#include <tools/tests/ut.h>
+#include <tools/http_error.h>
 
 #include <jsonrpccpp/server/connectors/httpserver.h>
 
@@ -24,76 +24,129 @@ MapServer::MapServer(
 
 
 Json::Value MapServer::appear() {
-    auto val = Json::Value{};
-    val["id"] = this->world.appear().id;
-    return val;
+    auto result = Json::Value(Json::objectValue);
+    try {
+        result["id"] = this->world.appear().id;
+    } catch (const HTTPError& err) {
+        throw jsonrpc::JsonRpcException(
+            err.code(), err.message()
+        );
+    }
+    return result;
 }
 
-int MapServer::front_move(int direction, int id) {
-    this->world.move(
-        Map::ObjectId(id),
-        Map::RelativeDirection(direction),
-        Map::World::Side::Front
-    );
-    return 0;
+Json::Value MapServer::front_move(int direction, int id) {
+    auto result = Json::Value(Json::objectValue);
+    try {
+        this->world.move(
+            Map::ObjectId(id),
+            Map::RelativeDirection(direction),
+            Map::World::Side::Front
+        );
+    } catch (const HTTPError& err) {
+        throw jsonrpc::JsonRpcException(
+            err.code(), err.message()
+        );
+    }
+    return result;
 }
 
-int MapServer::back_move(int direction, int id) {
-    this->world.move(
-        Map::ObjectId(id),
-        Map::RelativeDirection(direction),
-        Map::World::Side::Back
-    );
-    return 0;
+Json::Value MapServer::back_move(int direction, int id) {
+    auto result = Json::Value(Json::objectValue);
+    try {
+        this->world.move(
+            Map::ObjectId(id),
+            Map::RelativeDirection(direction),
+            Map::World::Side::Back
+        );
+    } catch (const HTTPError& err) {
+        throw jsonrpc::JsonRpcException(
+            err.code(), err.message()
+        );
+    }
+    return result;
 }
 
-int MapServer::pick_ut_front(int direction, int id) {
-    this->world.pickUpGrain(
-        Map::ObjectId(id),
-        Map::RelativeDirection(direction),
-        Map::World::Side::Front
-    );
-    return 0;
+Json::Value MapServer::pick_ut_front(int direction, int id) {
+    auto result = Json::Value(Json::objectValue);
+    try {
+        this->world.pickUpGrain(
+            Map::ObjectId(id),
+            Map::RelativeDirection(direction),
+            Map::World::Side::Front
+        );
+    } catch (const HTTPError& err) {
+        throw jsonrpc::JsonRpcException(
+            err.code(), err.message()
+        );
+    }
+    return result;
 }
 
-int MapServer::drop_front(int id) {
-    this->world.dropGrain(
-        Map::ObjectId(id),
-        Map::World::Side::Front
-    );
-    return 0;
+Json::Value MapServer::drop_front(int id) {
+    auto result = Json::Value(Json::objectValue);
+    try {
+        this->world.dropGrain(
+            Map::ObjectId(id),
+            Map::World::Side::Front
+        );
+    } catch (const HTTPError& err) {
+        throw jsonrpc::JsonRpcException(
+            err.code(), err.message()
+        );
+    }
+    return result;
 }
 
 Json::Value MapServer::look_to(int direction, int id, int segment) {
-    auto cell = this->world.lookTo(
-        Map::ObjectId(id),
-        Map::RelativeDirection(direction),
-        static_cast<size_t>(segment)
-    );
     auto val = Json::Value{};
-    val["owner_id"] = cell.objectId.id;
-    val["material"] = static_cast<int>(cell.grain);
-    return val;
-}
-
-Json::Value MapServer::get_pose(int id) {
-    auto pose = this->world.getPose(
-        Map::ObjectId(id)
-    );
-    auto val = Json::Value{};
-    auto& p = val["pose"];
-    p = Json::arrayValue;
-    for (const auto& dir : pose) {
-        p.append(dir.toInt());
+    try {
+        auto cell = this->world.lookTo(
+            Map::ObjectId(id),
+            Map::RelativeDirection(direction),
+            static_cast<size_t>(segment)
+        );
+        val["owner_id"] = cell.objectId.id;
+        val["material"] = static_cast<int>(cell.grain);
+    } catch (const HTTPError& err) {
+        throw jsonrpc::JsonRpcException(
+            err.code(), err.message()
+        );
     }
     return val;
 }
 
-int MapServer::ping(int id) {
-    this->world.ping(
-        Map::ObjectId(id)
-    );
-    return 0;
+Json::Value MapServer::get_pose(int id) {
+    auto val = Json::Value{};
+    try {
+        auto pose = this->world.getPose(
+            Map::ObjectId(id)
+        );
+        auto& p = val["pose"];
+        p = Json::arrayValue;
+        for (const auto& dir : pose) {
+            p.append(dir.toInt());
+        }
+    } catch (const HTTPError& err) {
+        throw jsonrpc::JsonRpcException(
+            err.code(), err.message()
+        );
+    }
+    return val;
+}
+
+Json::Value MapServer::ping(int id) {
+    auto result = Json::Value(Json::objectValue);
+    try {
+        this->world.ping(
+            Map::ObjectId(id)
+        );
+    } catch (const HTTPError& err) {
+        throw jsonrpc::JsonRpcException(
+            err.code(), err.message()
+        );
+    }
+    return result;
 }
 
 void MapServer::print(std::ostream& os) const {
--- ant_hill/map/app/map_server.h
+++ ant_hill/map/app/map_server.h
@@ -35,13 +35,13 @@ public:
     );
 
     Json::Value appear() override;
-    int front_move(int direction, int id) override;
-    int back_move(int direction, int id) override;
-    int pick_ut_front(int direction, int id) override;
-    int drop_front(int id) override;
+    Json::Value front_move(int direction, int id) override;
+    Json::Value back_move(int direction, int id) override;
+    Json::Value pick_ut_front(int direction, int id) override;
+    Json::Value drop_front(int id) override;
     Json::Value look_to(int direction, int id, int segment) override;
     Json::Value get_pose(int id) override;
-    int ping(int id) override;
+    Json::Value ping(int id) override;
 
 public:
     void print(std::ostream& os) const;
