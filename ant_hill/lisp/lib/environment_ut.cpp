0447faa5410a504c34676da477c26038bc9790d6
akindyakov <akindyakov@gmail.com>
2017-05-14 19:49:28 +0300

    empty files for env and parser module

---
+++ ant_hill/lisp/lib/environment_ut.cpp
@@ -0,0 +1,18 @@
+#include "environment.h"
+
+#include <tools/tests/ut.h>
+
+#include <iostream>
+
+
+int main() {
+    try {
+        std::cerr << "environment_ut:\n";
+        std::cerr << " - TODO\n";
+        std::cerr << std::endl;
+    } catch (const std::exception& except) {
+        std::cerr << "failed: " << except.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
---
+++ ant_hill/lisp/lib/parser_ut.cpp
@@ -0,0 +1,19 @@
+#include "parser.h"
+
+#include <tools/tests/ut.h>
+
+#include <iostream>
+
+
+int main() {
+    try {
+        std::cerr << "parser_ut:\n";
+        std::cerr << " - TODO\n";
+        std::cerr << std::endl;
+    } catch (const std::exception& except) {
+        std::cerr << "failed: " << except.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
