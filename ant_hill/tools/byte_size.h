69c4ea669ea9e5e7c656bce2571fa255b1674abb
akindyakov <akindyakov@gmail.com>
2017-06-23 13:11:04 +0300

    add conception of size definition

---
+++ ant_hill/tools/byte_size.h
@@ -0,0 +1,8 @@
+#pragma once
+
+constexpr size_t Byte_Byte = 1;
+
+constexpr size_t KiB_Byte = 1 << 10;
+constexpr size_t MiB_Byte = 1 << 20;
+constexpr size_t GiB_Byte = 1 << 30;
+constexpr size_t TiB_Byte = 1 << 40;
