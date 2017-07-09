8680ef4b05011eb91472321fabca2e5396056609
akindyakov <akindyakov@gmail.com>
2017-07-09 18:34:14 +0300

    use namespace instead of making ugly the constant names

--- ant_hill/lib/byte_size.h
+++ ant_hill/lib/byte_size.h
@@ -1,8 +1,13 @@
 #pragma once
 
-constexpr size_t Byte_Byte = 1;
+namespace Lib {
+namespace Bytes {
 
-constexpr size_t KiB_Byte = 1 << 10;
-constexpr size_t MiB_Byte = 1 << 20;
-constexpr size_t GiB_Byte = 1 << 30;
-constexpr size_t TiB_Byte = 1 << 40;
+constexpr size_t Byte = 1;
+constexpr size_t KiB = 1 << 10;
+constexpr size_t MiB = 1 << 20;
+constexpr size_t GiB = 1 << 30;
+constexpr size_t TiB = 1 << 40;
+
+}  // namespace Bytes
+}  // namespace Lib
