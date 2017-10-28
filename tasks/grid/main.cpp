dce516502463e8f1fab70e4cb520d56213d39b1f
akindyakov <akindyakov@gmail.com>
2017-10-28 17:07:32 +0300

    solution

--- tasks/grid/main.cpp
+++ tasks/grid/main.cpp
@@ -18,7 +18,8 @@ public:
         std::size_t x
         , std::size_t y
     ) const {
-        return grid[y * xSize + x];
+        //std::cerr << x << ":" << xSize << " " << y << ":" << ySize << '\n';
+        return grid.at(y * xSize + x);
     }
 
     std::size_t xSize;
@@ -26,48 +27,37 @@ public:
     std::vector<int> grid;
 };
 
-int count(const TGrid& grid) {
-    auto prev = std::vector<int>(grid.xSize);
-    auto counter = int{0};
+int Count(const TGrid& grid) {
+    auto prev = std::vector<int>(grid.xSize, 0);
     auto count = int{0};
     auto uid = int{0};
-    for (auto x = std::size_t{0}; x < grid.xSize; ++x) {
-        if (grid.at(x, 0)) {
-            ++count;
-            ++uid;
-            while (grid.at(x, 0)) {
-                prev[x] = uid;
-                ++x;
-            }
-        }
-    }
-    for (auto y = std::size_t{1}; y < grid.ySize; ++y) {
+    for (auto y = std::size_t{0}; y < grid.ySize; ++y) {
         for (auto x = std::size_t{0}; x < grid.xSize; ++x) {
             if (grid.at(x, y)) {
-                auto currUid = prev[x];
-                if (0 == prev[x]) {
+                auto currUid = prev.at(x);
+                if (0 == currUid) {
                     currUid = ++uid;
                     ++count;
+                    prev.at(x) = currUid;
                 }
-                while (x < grid.xSize && 0 != grid.at(x, y)) {
-                    if (prev[x] != currUid) {
-                        if (prev[x] == 0) {
-                            prev[x] = currUid;
+                while (++x < grid.xSize && 0 != grid.at(x, y)) {
+                    if (prev.at(x) != currUid) {
+                        if (prev.at(x) == 0) {
+                            prev.at(x) = currUid;
                         } else {
                             --count;
-                            auto oldUid = prev[x];
+                            auto oldUid = prev.at(x);
                             for (auto i = x; i < grid.xSize; ++i) {
-                                if (oldUid == prev[i]) {
-                                    prev[i] = currUid;
+                                if (oldUid == prev.at(i)) {
+                                    prev.at(i) = currUid;
                                 }
                             }
                         }
                     }
-                    ++x;
                 }
-                --x;
+                --x;  // first zero(0) after non-zero sequence - if it is not out of range -> mark value to zero
             } else {
-                prev[x] = 0;
+                prev.at(x) = 0;
             }
         }
     }
@@ -85,10 +75,12 @@ int main() {
         };
         grid.xSize = 4;
         grid.ySize = 4;
-        auto ans = count(grid);
+        auto ans = Count(grid);
         if (ans != 3) {
             std::cerr << "0: failed " << ans << " != 3" << '\n';
             throw std::exception();
+        } else {
+            std::cerr << "0: success\n";
         }
     }
     {
@@ -100,11 +92,13 @@ int main() {
             0,0,1,1,1,
         };
         grid.xSize = 5;
-        grid.ySize = 5;
-        auto ans = count(grid);
+        grid.ySize = 4;
+        auto ans = Count(grid);
         if (ans != 1) {
             std::cerr << "1: failed " << ans << " != 1" << '\n';
             throw std::exception();
+        } else {
+            std::cerr << "1: success\n";
         }
     }
     {
@@ -117,10 +111,12 @@ int main() {
         };
         grid.xSize = 5;
         grid.ySize = 4;
-        auto ans = count(grid);
+        auto ans = Count(grid);
         if (ans != 10) {
             std::cerr << "2: failed " << ans << " != 10" << '\n';
             throw std::exception();
+        } else {
+            std::cerr << "2: success\n";
         }
     }
     {
@@ -133,10 +129,12 @@ int main() {
         };
         grid.xSize = 4;
         grid.ySize = 4;
-        auto ans = count(grid);
+        auto ans = Count(grid);
         if (ans != 2) {
             std::cerr << "3: failed " << ans << " != 2" << '\n';
             throw std::exception();
+        } else {
+            std::cerr << "3: success\n";
         }
     }
     {
@@ -149,10 +147,58 @@ int main() {
         };
         grid.xSize = 4;
         grid.ySize = 4;
-        auto ans = count(grid);
+        auto ans = Count(grid);
         if (ans != 1) {
             std::cerr << "4: failed " << ans << " != 2" << '\n';
             throw std::exception();
+        } else {
+            std::cerr << "4: success\n";
+        }
+    }
+    {
+        auto grid = TGrid(0, 0);
+        grid.grid = std::vector<int>{
+            1,0,0,0,1,
+            1,1,1,0,1,
+            0,0,1,0,1,
+            1,0,1,0,1,
+            1,0,1,0,1,
+            1,1,1,1,1,
+        };
+        grid.xSize = 5;
+        grid.ySize = 6;
+        auto ans = Count(grid);
+        if (ans != 1) {
+            std::cerr << "5: failed " << ans << " != 1" << '\n';
+            throw std::exception();
+        } else {
+            std::cerr << "5: success\n";
+        }
+    }
+    {
+        auto grid = TGrid(0, 0);
+        grid.grid = std::vector<int>{
+            1,0,0,0,1,0,0,0,0,1,
+            1,1,1,0,1,0,1,1,0,1,
+            0,0,1,0,1,0,0,1,0,1,
+            1,0,1,0,1,0,0,1,0,1,
+            1,0,1,0,1,0,0,1,0,1,
+            1,1,1,1,1,0,1,1,1,1,
+            0,0,0,0,1,0,0,0,0,1,
+            1,1,1,0,1,1,1,1,0,1,
+            0,0,1,0,0,0,0,1,0,1,
+            1,0,1,0,1,0,0,1,0,1,
+            1,0,1,0,0,1,0,1,0,1,
+            1,1,1,0,0,0,0,1,1,1,
+        };
+        grid.xSize = 10;
+        grid.ySize = 12;
+        auto ans = Count(grid);
+        if (ans != 4) {
+            std::cerr << "5: failed " << ans << " != 4" << '\n';
+            throw std::exception();
+        } else {
+            std::cerr << "5: success\n";
         }
     }
 }
