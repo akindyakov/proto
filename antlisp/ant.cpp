c9eb71f067279bbacba632cdefa6c49fae09d772
akindyakov <akindyakov@gmail.com>
2018-02-03 20:37:07 +0300

    run machine

--- antlisp/ant.cpp
+++ antlisp/ant.cpp
@@ -3,72 +3,245 @@
 #include <vector>
 #include <unordered_map>
 #include <exception>
+#include <memory>
+
+#include <boost/variant.hpp>
+#include <boost/variant/get.hpp>
 
 
-using TValue = int;
+class FunctionDefinition;
+class FunctionCall;
+
+using FunctionDefinitionPtr =    std::shared_ptr<FunctionDefinition>;
+using PostponedFunctionCallPtr = std::shared_ptr<FunctionCall>;
+
+struct Nil {};
+
+using Cell = boost::variant<
+    Nil
+    , int
+    , std::string
+    , FunctionDefinitionPtr
+    , PostponedFunctionCallPtr
+>;
+
+bool isNil(const Cell& cell) {
+    return cell.which() == 0;
+}
 
 // may we use stringview here?
 using TVarName = std::string;
 
-struct GlobalScope {
-    std::unordered_map<TVarName, TValue> vars;
-};
+using LocalFrame = std::vector<Cell>;
+using GlobalFrame = std::unordered_map<TVarName, Cell>;
 
-struct LocalScope {
-    std::size_t argsNumber;
-    std::vector<TValue> stack;
-    std::vector<TVarName> varNames;
-};
+struct Environment;
 
 struct FunctionDefinition {
     enum EOperations {
-        GetLocalFunction,     // Где брать функцию? Из локального скопа значений по номеру. Клдем указатель на функцию в стек.
-        GetGlobalFunction,    // Где брать функцию? Из глобального словаря функций по имени лежащему в локальном скоупе имен по номеру.
+        Nope,
+        //UnpackFunction,     // Берем последнее значение со стека и превращаем его в вызываемый объект со стеком. лдем указатель на функцию в стек.
         // Для любой функции: создаем новый LocalScope - кладем его в специальный стек.
-        GetLocalVar,          // Берем из локального скоупа по номеру в стеке.
-        GetGlobalVar,         // Берез из глобального скоупа по имени, которое берем по номеру из локального скоупа имен.
-        RunFunction,    // Запуск функции
+        //GetLocal,          // Берем из локального скоупа по номеру в стеке.
+        GetGlobal,         // Берез из глобального скоупа по имени, которое берем по номеру из локального скоупа имен.
+        SetGlobal,
+        RunFunction,          // Запуск функции, сколько аргументов положить в функцию, а так же где эту самую функцию взять
+        RunExternalFunction,  // Запуск внешней по отношению к языку функции (встроеные и притянутые из CPP функции)
         StackRewind,    // Сколько нужно отмотать?
-        Return,         // Что возвращаем? По сути явно перекладывает нужное значение на вершину стека
+        SkipIfTrue,
+        SkipIfFalse,
     };
     struct Step {
         EOperations operation;
         std::size_t position = 0;
     };
-    std::vector<Step> script;
+    std::vector<Step> operations;
+    std::size_t ArgsNumber;
+    std::vector<TVarName> VarNames;
+
+    static bool step(Environment& env);
 };
 
-struct FunctionCall {
-    LocalScope scope;
-    FunctionDefinition function;  // FIXME: use pointer here!
-    std::size_t scriptRunner = 0;
-
-    FunctionDefinition::EOperations step() const {
-        return function.script[scriptRunner].operation;
+class FunctionCall {
+public:
+    explicit FunctionCall(
+        FunctionDefinitionPtr fdef
+        , LocalFrame frame_
+    )
+        : function(
+            std::move(fdef)
+        )
+        , frame(
+            std::move(frame_)
+        )
+    {
     }
 
-    TValue getLocalVar() const {
-        return scope.stack[
-            function.script[scriptRunner].position
-        ];
-    }
-
-    TValue getGlobalVarName() const {
-        return scope.varNames[
-            function.script[scriptRunner].position
-        ];
-    }
-
-    void rewind() {
-        return scope.stack.resize(
-            fscope.stack.size() - unction.script[scriptRunner].position
-        );
+    FunctionDefinition::EOperations getOperation() const {
+        return function->operations[runner].operation;
     }
 
     bool next() noexcept {
-        return ++scriptRunner < function.script.size();
+        return ++runner < function->operations.size();
+    }
+
+    Cell popLocal() {
+        auto value = std::move(
+            frame.back()
+        );
+        frame.pop_back();
+        return value;
+    }
+
+    void pushLocal(Cell cell) {
+        frame.push_back(
+            std::move(cell)
+        );
+    }
+
+    const Cell& getLocal() const {
+        return frame[
+            function->operations[runner].position
+        ];
+    }
+
+    void getGlobal(const GlobalFrame& global) {
+        const auto& name = function->VarNames[
+            function->operations[runner].position
+        ];
+        this->pushLocal(
+            global.at(name)
+        );
+    }
+
+    void setGlobal(GlobalFrame& global) {
+        const auto& name = function->VarNames[
+            function->operations[runner].position
+        ];
+        global[name] = this->popLocal();
+    }
+
+    void rewind() {
+        frame.resize(
+            frame.size() - function->operations[runner].position
+        );
+    }
+
+    void skip() {
+        runner += function->operations[runner].position;
+    }
+
+    LocalFrame createNewFrame() {
+        auto size = function->operations[runner].position;
+        auto newFrame = LocalFrame(size);
+        while (size--) {
+            newFrame[size] = popLocal();
+        }
+        return newFrame;
+    }
+
+private:
+    void setGlobal(GlobalFrame& global, Cell cell) const {
+        const auto& name = function->VarNames[
+            function->operations[runner].position
+        ];
+        global[name] = std::move(cell);
+    }
+
+private:
+    LocalFrame frame;
+    FunctionDefinitionPtr function;
+    std::size_t runner = 0;
+};
+
+struct Environment {
+    std::vector<FunctionCall> CallStack;
+    std::unordered_map<TVarName, Cell> vars;
+
+    bool isStackEmpty() {
+        return this->CallStack.empty();
+    }
+
+    FunctionCall* stackTop() {
+        return &this->CallStack.back();
+    }
+
+    FunctionCall* stackPush(
+        FunctionCall frame
+    ) {
+        this->CallStack.push_back(
+            std::move(frame)
+        );
+        return this->stackTop();
+    }
+
+    void stackPop() {
+        return this->CallStack.pop_back();
     }
 };
+
+bool FunctionDefinition::step(Environment& env) {
+    auto call = env.stackTop();
+    switch (call->getOperation()) {
+        case FunctionDefinition::Nope:
+            break;
+        case FunctionDefinition::GetGlobal:
+            call->getGlobal(env.vars);
+            break;
+        case FunctionDefinition::SetGlobal:
+            call->setGlobal(env.vars);
+            break;
+        case FunctionDefinition::RunFunction:
+            {
+                auto frame = call->createNewFrame();
+                auto fdef = boost::get<FunctionDefinitionPtr>(
+                    call->popLocal()
+                );
+                call = env.stackPush(
+                    FunctionCall(
+                        std::move(fdef),
+                        std::move(frame)
+                    )
+                );
+            }
+            break;
+        case FunctionDefinition::RunExternalFunction:
+            // TODO:
+            break;
+        case FunctionDefinition::StackRewind:
+            call->rewind();
+            break;
+        case FunctionDefinition::SkipIfTrue:
+            {
+                auto guard = call->popLocal();
+                if (isNil(guard)) {
+                    call->skip();
+                }
+            }
+            break;
+        case FunctionDefinition::SkipIfFalse:
+            {
+                auto guard = call->popLocal();
+                if (!isNil(guard)) {
+                    call->skip();
+                }
+            }
+            break;
+    }
+    if (!call->next()) {
+        auto ret = call->popLocal();
+        env.stackPop();
+        if (env.isStackEmpty()) {
+            return false;
+        }
+        call = env.stackTop();
+        call->pushLocal(
+            std::move(ret)
+        );
+    }
+    return true;
+}
+
 
 /**
 * Как быть с заполнением let ?
@@ -97,30 +270,7 @@ struct FunctionCall {
 */
 
 int main() {
-    auto global = GlobalScope{};
-    auto stack = std::vector<FunctionCall>{};
-    stack.push_back(
-        FunctionCall{}
-    );  // Define global todo list
-    auto stackPtr = std::size_t{0};
-    while (true) {
-        auto& fCall = stack[stackPtr];
-        switch (fCall.step()) {
-            case GetLocalFunction:
-                auto value = fCall.getLocalVar();
-                break;
-            case GetGlobalFunction:
-                break;
-            case GetLocalVar:
-                break;
-            case GetGlobalVar:
-                break;
-            case RunFunction:
-                break;
-            case StackRewind:
-                break;
-            case Return;
-                break;
-        }
+    Environment env;
+    while (FunctionDefinition::step(env)) {
     }
 }
